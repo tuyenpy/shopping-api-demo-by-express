71fdb71e175af59657353173e2e38a9e55e21210
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-03-21 21:15:27 +0700

    Add: create and login user

---
+++ api/config/auth.js
@@ -0,0 +1,30 @@
+// const express = require('express');
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.secretKey || "secretKey";
+
+module.exports.isAuth = (req, res, next) => {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if (typeof bearerHeader !== 'undefined'){
+        // Split at the space
+        const bearer = bearerHeader.split(' ');
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token
+        //req.token = bearerToken;
+        jwt.verify(bearerToken, secretKey, (err, data) => {
+            if (err) {
+                res.sendStatus(403);
+            } else {
+                //res.json(data);
+                // Next middleware
+                next();
+            }
+        });
+
+    } else {
+    // Forbidden
+    res.sendStatus(403);
+    }
+};
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 
 const User = require('../../models/User');
+const {isAuth} = require('../config/auth');
 const secretKey = process.env.secretKey || "secretKey";
 
+//Login
 router.post('/login', (req, res) => {
     const {email, password} = req.body;
     User.findOne({email: email})
@@ -26,9 +28,49 @@ router.post('/login', (req, res) => {
     });
 });
 
+//Create user
 router.post('/register', (req, res) => {
-    const {name, phone, email, password} = req.body;
-});
+    const {name, phone, email, password, password1} = req.body;
+    const errors = [];
+    if (!name || !phone || !email || !password){
+        errors.push({msg: "Please enter all fields"});
+    }
 
+    if (password !== password1){
+        errors.push({msg: "Password do not match"});
+    }
+
+    if (errors.length > 0){
+        res.json(errors);
+    } else {
+        User.findOne({email: email})
+        .then(user => {
+            if (user) {
+                res.json({
+                    msg: "User is already exits",
+                    user: user
+                });
+            }
+        })
+        .catch(err => console.log(err));
+
+        const newUser = new User({
+            name,
+            phone,
+            email,
+            password
+        });
+
+        newUser.save()
+        .then(user => {
+            res.json({
+                msg: "User is created",
+                user: user
+            });
+        })
+        .catch(err => console.log(`POST /register: `, err));
+    }
+
+});
 
 module.exports = router;
